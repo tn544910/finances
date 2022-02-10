320e22a98d8451a214de33d9ac11fcd2b74139bd
Thanh <thaaanh.nguyen@gmail.com>
2022-02-10 22:53:16 +0100

    added table and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
-import "./App.css";
+import "./app.css";
 import { Table } from "./table";
+
+const incomeData: Table = {
+  header: { name: "Einkommen", amount: "Betrag" },
+  rows: [
+    { name: "Arbeit", amount: 2637 },
+    { name: "DAZN", amount: 5 },
+  ],
+};
+
+const expenseData: Table = {
+  header: { name: "Ausgaben", amount: "Betrag" },
+  rows: [
+    { name: "Miete", amount: 680 },
+    { name: "Strom", amount: 37 },
+    { name: "Gas", amount: 14 },
+  ],
+};
 
 function App() {
   return (
-    <div className="App">
-      <Table />
+    <div className="app">
+      <div className="app-title">FINANZEN</div>
+      <Table header={incomeData.header} rows={incomeData.rows} />
+      <Table header={expenseData.header} rows={expenseData.rows} />
     </div>
   );
 }
--- src/table.tsx
+++ src/table.tsx
@@ -1,13 +1,89 @@
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
+import "./table.css";
 
-export function Table(): ReactElement {
+export interface Table {
+  header: HeaderRow;
+  rows: BodyRow[];
+}
+
+export function Table({ header, rows }: Table): ReactElement {
   return (
-    <div>
-      <Row />
+    <div className="table">
+      <HeaderRow name={header.name} amount={header.amount} />
+      {rows.map((row, index) => (
+        <BodyRow key={index} name={row.name} amount={row.amount} />
+      ))}
     </div>
   );
 }
 
-function Row(): ReactElement {
-  return <div>1</div>;
+interface HeaderRow {
+  name: string;
+  amount: string;
+}
+
+interface BodyRow {
+  name: string;
+  amount: number;
+}
+
+interface PureRow {
+  type: "header" | "body";
+  children?: ReactNode;
+}
+
+function HeaderRow({ name, amount }: HeaderRow): ReactElement {
+  return (
+    <Row type="header">
+      <HeaderCell value={name} />
+      <HeaderCell value={amount} />
+    </Row>
+  );
+}
+
+function BodyRow({ amount, name }: BodyRow): ReactElement {
+  return (
+    <Row type="body">
+      <NameCell value={name} />
+      <CurrencyCell value={amount} />
+    </Row>
+  );
+}
+
+function Row({ type, children }: PureRow): ReactElement {
+  return <div className={`table-row-${type}`}>{children}</div>;
+}
+
+interface StringCell {
+  value: string;
+}
+
+interface CurrencyCell {
+  value: number;
+}
+
+function HeaderCell({ value }: StringCell) {
+  return <PureCell value={value} type="header" />;
+}
+
+function NameCell({ value }: StringCell): ReactElement {
+  return <PureCell value={value} type="name" />;
+}
+
+function CurrencyCell({ value }: CurrencyCell): ReactElement {
+  return <PureCell value={toEuro(value)} type="currency" />;
+}
+
+interface PureCell {
+  value: string;
+  type: "header" | "name" | "currency";
+}
+
+function PureCell({ value, type }: PureCell): ReactElement {
+  return <div className={`table-cell-${type}`}>{value}</div>;
+}
+
+function toEuro(value: number): string {
+  const withDecimals = value.toFixed(2);
+  return `${withDecimals} €`;
 }
